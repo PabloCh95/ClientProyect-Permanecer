6e1ad3c0d916603f702b339c627dfa6c36567718
PabloCh95 <58525405+PabloCh95@users.noreply.github.com>
2020-10-02 12:35:47 -0300

    subiendo archivos iniciales

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Navigation from "./src/Navigations/Navigation";//importamos navigation
+import AuthProvider from './src/provider/AuthProvider';
+
+export default function App() {
+  return (
+      <Navigation /> //aplicamos navigation
+  );
+}
